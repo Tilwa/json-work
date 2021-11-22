6a47cff8e9b923b63382087195a702a5ad27270a
Shahruhkh altaf <shahrukhaltaf123@gmail.com>
2021-11-22 10:59:41 +0500

    2nd commit

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -7,7 +7,7 @@ class FullPost extends Component {
         let post = <p>Please select a Post!</p>;
         post = (
             <div className="FullPost">
-                <h2>Title</h2>
+                <h1>Title</h1>
                 <p>Content</p>
                 <div className="Edit">
                     <button className="Delete">Delete</button>
